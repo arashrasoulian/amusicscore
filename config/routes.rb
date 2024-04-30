ad56c67ef3b4ce90388e5d8345459571d5194d21
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-04-30 19:37:43 +0200

    add api/v1 to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :scores
+      resources :storing
+    end
+  end
+
 end
